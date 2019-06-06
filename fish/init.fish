0836a15f9b6dbe331604ac9e18de60bbb75f7cdb
David Aerne <meodai@gmail.com>
2019-06-06 21:54:59 +0200

    Adds fish config

---
+++ fish/init.fish
@@ -0,0 +1,15 @@
+# exports
+# locale
+set -xU LC_CTYPE en_US.UTF-8
+set -xU LC_ALL en_US.UTF-8
+
+set -xU TERM xterm-color
+set -xU CLICOLOR 1
+
+set -xU HOMEBREW_CASK_OPTS "--appdir=/Applications"
+
+# set path vars
+# Python 3
+#set -gx PATH $PATH "/Library/Frameworks/Python.framework/Versions/3.4/bin"
+# GNU core utilities
+set -gx PATH $PATH (brew --prefix coreutils)/libexec/gnubin:/usr/local/bin
